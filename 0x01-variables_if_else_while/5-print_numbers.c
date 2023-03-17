d24a4ee4f366e3a8787bbcc6342393bd231efabb
Mohamed KADI <handskadi@gmail.com>
2023-03-17 16:12:33 -0700

    Base 10  printing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` to print lowercase alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while ( n < 10 )
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
