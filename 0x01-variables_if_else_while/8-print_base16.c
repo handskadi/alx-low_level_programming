c22b7eae772f1bdd09c5e34d543d73b14889f233
Mohamed KADI <handskadi@gmail.com>
2023-03-17 17:14:41 -0700

    fix inmy magic code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,20 +11,14 @@ int main(void)
 
         while (n < 10)
         {
-		if (n >= 9)
-		{
-			while(c <= 'g')
-			{
-				putchar(c);
-				c++;
-			}
-		} else
-		{
-			putchar(n + '0');
-			n++;
-		}
+		putchar(n + '0');
+		n++;
         }
-
+	while(c <= 'g')
+	{
+		putchar(c);
+		c++;
+	}
         putchar('\n');
 
         return (0);
