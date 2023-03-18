372657157e828c4f66e7776b938b31cd6ec90971
Mohamed KADI <handskadi@gmail.com>
2023-03-17 17:02:59 -0700

    My maagic code ...

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` to print lowercase alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+        int long n = 0;
+	char c = 'a';
+
+        while (n < 10)
+        {
+		if (n >9)
+		{
+			while(c <= 'g')
+			{
+				putchar(c);
+				c++
+			}
+		} else
+		{
+			putchar(n + '0');
+			n++;
+		}
+        }
+
+        putchar('\n');
+
+        return (0);
+}
