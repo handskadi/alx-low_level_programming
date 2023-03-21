e2daee23a203d1e2eca4450914aef3e55d5a575d
Mohamed KADI <handskadi@gmail.com>
2023-03-21 16:57:05 -0700

    initial nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int nl = 0;
+	char al;
+
+	while (nl <= 10)
+	{
+		al = 'a'
+		while (al <= 'z')
+		{
+			_putchar(al);
+			al++;
+		}
+		_putchar('\n');
+		nl++;
+	}
+}
