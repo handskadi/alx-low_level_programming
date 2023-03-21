dafc794ba225b8ef060d7f0e9a47bc0353a6f6ac
Mohamed KADI <handskadi@gmail.com>
2023-03-21 16:41:51 -0700

    Fix number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry point
- * description: program that prints all alphabets in lowercase
- * Return: Always 0 (Success_)
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
-/**
  * print_alphabet - entry point
  * description: function loops letters
  * Return: void
