bfa5579a0ad30e10c6a0393d541ed1ae85781f05
Mohamed KADI <handskadi@gmail.com>
2023-03-21 15:43:39 -0700

    first task , first attempt today

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * description: program that prints _putchar
+ * @void: emprty parameter
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+* Functions and nested loops
+
+In the following you will find all the projects done on C functions and loops (Alx software Engineering)
+** GitHub repository:
+- alx-low_level_programming
+
+** Directory: 
+- 0x02-functions_nested_loops
