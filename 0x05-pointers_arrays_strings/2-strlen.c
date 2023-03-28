5405fada07c6c8fd28bc7298c2a2ae9aa040cabb
Mohamed KADI <handskadi@gmail.com>
2023-03-28 03:17:02 -0700

    task 02: print length of a sting : Fix 3(return a)

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,14 @@
  * _strlen -  Function that returns the length of a string
  * Returns number of char in a string
  * @s: string to be counted
+ * Return: a ( Number of char in a string )
  */
 int _strlen(char *s)
 {
 	int a;
 
 	a = 0;
-	while(s[a] != '\0')
+	while (s[a] != '\0')
 	{
 		a++;
 	}
