eab5676918c322ada33f3b39a706f66a308dbbb1
Mohamed KADI <handskadi@gmail.com>
2023-04-10 07:29:18 -0700

    Program Iminit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - MK prints the name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
