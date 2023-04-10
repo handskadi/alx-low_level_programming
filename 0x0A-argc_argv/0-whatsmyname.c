effa38b79dfac87792d62926229b320a06561bb1
Mohamed KADI <handskadi@gmail.com>
2023-04-10 07:45:41 -0700

    fix x"

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - MK : prints the name of a program
- * @argc: number of arguments
+ * main - prints the name of a program
+ * @argc: MK number of arguments
  * @argv: array of arguments
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	_putchar("%s\n", argv[argc * 0]);
+	printf("%s\n", argv[argc * 0]);
 
 	return (0);
 }
