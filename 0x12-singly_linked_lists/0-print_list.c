c94ff195e081ff351c6e1aed9bcbfef53e6e845a
Mohamed KADI <handskadi@gmail.com>
2023-04-27 07:08:27 -0700

    Task 0 fix 01

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,10 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - MK,  prints all the elements of a list_t
- * list.
+ * print_list - Mk, prints all the elements of a linked list
  * @h: pointer to the list_t list to print
+ *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
