a06063532b39ca65bb98b5b78b416a6cbf0122b4
Mohamed KADI <handskadi@gmail.com>
2023-04-27 18:48:48 -0700

    task 3, fix 1

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,17 +12,17 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int MKlen = 0;
+	unsigned int len = 0;
 
-	while (str[MKlen])
-		MKlen++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->MKlen = MKlen;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
