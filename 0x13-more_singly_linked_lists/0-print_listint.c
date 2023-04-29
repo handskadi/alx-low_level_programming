f4d57be25e7f898dc773f4ed6a47e6da4980d2f0
Mohamed KADI <handskadi@gmail.com>
2023-04-29 02:26:29 -0700

    Task 0 : Init

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - hadi ghadi tprinti lina kolchi les lement li kiynin f listint.
+ * @h: hadi tatchir l_isted link men type listlink
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
