c2a97399a5d5d3227672a3bbe56c383937f81ee3
Mohamed KADI <handskadi@gmail.com>
2023-04-29 02:33:23 -0700

    Task 1 : Init

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - f linkint list returni lina kolchi les elements
+ * @h: linked list dyal type d listint__t
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t Mk_number = 0;
+
+	while (h)
+	{
+		MK_number++;
+		h = h->next;
+	}
+
+	return (MK_number);
+}
