8b0bbe74dec52537defdd92e10c4df10d98fa4df
Mohamed KADI <handskadi@gmail.com>
2023-04-29 02:42:33 -0700

    Task 2 : Init

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - ajouti wa7d node f lbdia diyal la list .
+ * @head: tadgh datpointa sar node tamzgarot n list 
+ * @n: data n nchkcham agnso n node
+ * Return: anpointa sar node tojdid nghad Null ig walo
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *fresh;
+
+	fresh = malloc(sizeof(listint_t));
+	if (!fresh)
+		return (NULL);
+
+	fresh->n = n;
+	fresh->next = *head;
+	*head = fresh;
+
+	return (fresh);
+}
