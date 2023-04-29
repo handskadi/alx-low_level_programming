2f4699a02a81996597068d9a67a44b93cb868078
Mohamed KADI <handskadi@gmail.com>
2023-04-29 02:55:22 -0700

    Task 4 : Init

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - vider la list 
+ * @head: la list li ghadi tvida
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
