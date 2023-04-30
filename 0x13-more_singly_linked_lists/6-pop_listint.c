a90e4d63fe1df0bfcacd23ceee05b556ffddd30b
Mohamed KADI <handskadi@gmail.com>
2023-04-29 17:05:13 -0700

    task 06: init

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * pop_listint - sofghd element amzgarou g lalist iga linked 
+ * @head: Pointer sar  la list
+ * Return: L9ima diyal 
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *t;
+	int ls_value;
+
+	if (!(*head != NULL))
+		return (0);
+
+	t = *head;
+	*head = t->next;
+	ls_value = t->n;
+	free(t);
+	return (ls_value);
+}
