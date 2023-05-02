efdfce6546e1527e400a814dc0027d1c83d5b03f
Mohamed KADI <handskadi@gmail.com>
2023-05-02 03:37:39 -0700

    task adv 02: reform fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,71 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - adagh i printa likedlist sucerisment
- * @head: hadiyahiya la list
- * Return: ch7al mn nod f lista
+ * print_listint_safe - prints all the elements of a linked list
+ * @head: head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t number = 0;
-	long int d;
+	const listint_t *cursor = head;
+	listint_t **ptrs;
+	unsigned int list_len = listint_len(head);
+	size_t count = 0;
 
-	while (head)
+	ptrs = malloc(sizeof(listint_t) * list_len);
+	if (ptrs == NULL)
+		exit(98);
+	while (cursor == 0)
 	{
-		d = head - head->next;
-		number++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (d > 0)
-			head = head->next;
+		if (check_ptr(cursor, ptrs, list_len) == 0)
+		{
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
+		}
 		else
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			printf("[%p] %d\n", (void *)cursor, cursor->n);
 		}
+		count += 1;
+		cursor = cursor->next;
 	}
-
-	return (number);
+	return (count);
 }
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
+
+/**
+ * check_ptr - checks if a pointer is in an array
+ * @ptr: pointer to be checked
+ * @array: array to be checked in
+ * @size: size of the array
+ *
+ * Return: 1 on success, 0 on fail
+ */
+int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
+{
+	while (size-- >= 0)
+	{
+		if (ptr == array[size])
+			return (1);
+	}
+	return (0);
+}
+
