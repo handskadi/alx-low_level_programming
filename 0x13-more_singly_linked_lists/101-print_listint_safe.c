81813826a95f0fb3a8005f6fd7c9a01ce92b172f
Mohamed KADI <handskadi@gmail.com>
2023-05-02 03:31:12 -0700

    task adv 02: Betty fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,7 @@
 /**
  * print_listint_safe - adagh i printa likedlist sucerisment
  * @head: hadiyahiya la list
- * Return: ch7al mn nod f lista 
+ * Return: ch7al mn nod f lista
  */
 size_t print_listint_safe(const listint_t *head)
 {
