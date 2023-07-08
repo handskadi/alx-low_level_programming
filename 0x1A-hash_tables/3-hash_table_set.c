92521ed72ac8ed19f0feb9abd38f9198779441b1
Mohamed KADI <handskadi@gmail.com>
2023-07-08 09:50:34 +0000

    Task 3: fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *val_cpy;
+	char *val_cpy;;
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
